67271318602bf0f15d9d979c4b1b7996e9deb4af
Marcelo Medrado <celo1402ang@hotmail.com>
2024-11-29 17:16:41 -0300

    commit all

--- LoggedAndLogouted/Program.cs
+++ LoggedAndLogouted/Program.cs
@@ -69,6 +69,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+
 var app = builder.Build();
 
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
